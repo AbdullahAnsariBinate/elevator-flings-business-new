35cbef77c21aceb9a8b5f758588b215b8c10ab1b
AbdullahAnsariBinate <abdullah.ansari@binatedigital.com>
2023-05-30 03:55:49 +0500

    splash done

--- App.js
+++ App.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import Root from './src/routing/Root';
 import Auth from './src/routing/Auth';
 import { useSelector } from 'react-redux';
+import SplashScreen from "react-native-splash-screen";
 
 const App = () => {
-  const reduxState = useSelector(({ auth  , root , language}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      SplashScreen.hide();
+    }, 3000);
+
+  }, []);
+
+  const reduxState = useSelector(({ auth, root, language }) => {
     console.log("🚀 ~ file: App.js ~ line 13 ~ reduxState ~ auth", auth);
     return {
-        getUserProfileLoading: auth.getUserProfileLoading,
-        isLoggedIn: auth.isLoggedIn,
-        user: auth.user,
-        // language:language?.language?.lan
+      getUserProfileLoading: auth.getUserProfileLoading,
+      isLoggedIn: auth.isLoggedIn,
+      user: auth.user,
+      // language:language?.language?.lan
     };
-});
+  });
   console.log("🚀 ~ file: App.js:17 ~ reduxState ~ reduxState:", reduxState)
   const renderRouting = (value, initial) => {
     console.log("🚀 ~ file: App.js:18 ~ renderRouting ~ initial:", initial)
     console.log("🚀 ~ file: App.js:18 ~ renderRouting ~ value:", value)
     switch (value) {
       case true:
-        return <Root/>;
+        return <Root />;
       case false:
         return <Auth initial={initial} />;
       default:
